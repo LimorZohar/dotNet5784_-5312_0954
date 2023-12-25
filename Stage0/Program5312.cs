97e53642bd9f9f46304e63fb13b74849e4f4ba37
LimorZohar <limorz14123@gmail.com>
2023-12-25 16:36:21 +0200

    Stage 0 - final steps

---
+++ Stage0/Program5312.cs
@@ -0,0 +1,27 @@
+﻿using System;
+namespace Targil0
+{
+    partial class Program
+    {
+        static void Main(string[] args)
+        {
+            Welcome5312();
+
+            Console.ReadKey();
+        }
+
+        private static void Welcome5312()
+        {
+            Welcomr0954();
+        }
+
+        private static void Welcomr0954()
+        {
+            Console.WriteLine("Hello, World! it's me");
+            String name;
+            Console.WriteLine("Enter your name: ");
+            name = System.Console.ReadLine();
+            Console.WriteLine(name + ", welcome to my first application");
+        }
+    }
+}
