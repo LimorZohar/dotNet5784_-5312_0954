2bba1d702dacac5d0c4d55fc31ee05d3688c6cd9
hadas <hdsdwnt@gmail.com>
2023-12-27 19:56:38 +0200

    hey miamor

---
+++ Stage0/program0954.cs
@@ -0,0 +1,11 @@
+using System;
+namespace Targil0
+{
+    partial class program
+    {
+        static partial void welcome0954()
+        {
+            console.WriteLine("I am also here");
+        }
+    }
+}
