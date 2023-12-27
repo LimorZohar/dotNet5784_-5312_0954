71a8a0f3c29beadfdced0c945cc59e74f3ecb460
LimorZohar <limorz14123@gmail.com>
2023-12-27 19:48:21 +0200

    its ok

--- Stage0/Program5312.cs
+++ Stage0/Program5312.cs
@@ -1,4 +1,4 @@
-﻿using System;
+﻿using System;ConsoleKey
 namespace Targil0
 {
     partial class Program
@@ -12,16 +12,17 @@ namespace Targil0
 
         private static void Welcome5312()
         {
-            Welcomr0954();
+            Welcome0954();
         }
 
-        private static void Welcomr0954()
+        private static void Welcome0954()
         {
             Console.WriteLine("Hello, World! it's me");
             String name;
             Console.WriteLine("Enter your name: ");
             name = System.Console.ReadLine();
             Console.WriteLine(name + ", welcome to my first application");
+            
         }
     }
 }
