92936ca1a8f99cf9b4c27468cf69e1bfc657781e
hadas <hdsdwnt@gmail.com>
2023-12-27 23:32:36 +0200

    Stage0 - welcome0954

--- Stage0/Program.cs
+++ Stage0/Program.cs
@@ -1,8 +1,17 @@
 ﻿namespace Stage0;
-internal class Program
+partial class Program
 {
+     static partial void welcome5312();
     private static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        welcome0954();
+        welcome5312();
+    }
+
+    private static void welcome0954()
+    {
+        Console.WriteLine("Please give me your name");
+        string? userName = Console.ReadLine();
+        Console.WriteLine("Welcome,{0} to my World!",userName);
     }
 }
