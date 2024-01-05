6c2370c545afe9dd761f1608ac9928809e63c56e
hadas <hdsdwnt@gmail.com>
2024-01-05 08:04:44 +0200

    add a classes and interfaces ,enums

---
+++ DalList/DataSource.cs
@@ -0,0 +1,18 @@
+﻿
+namespace Dal;
+internal static class DataSource
+{
+    internal static class Config
+    {
+        internal const int startCourseId = 1000;
+        private static int nextCourseId = startCourseId;
+        internal static int NextCourseId { get => nextCourseId++; }
+    }
+       
+
+        internal static List<DO.Engineer> Engineers { get; } = new();//LIST OF ENGINEERS
+    internal static List<DO.Task> Tasks { get; } = new();//LIST OF TASK
+    internal static List<DO.Dependency> Dependencies { get; } = new();//LIST OF DEPENDENCY
+
+    //...
+}
