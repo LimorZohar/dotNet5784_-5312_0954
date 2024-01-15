2f2615df9a61dcd634e8e56f81014f4093c2dab2
hadas <hdsdwnt@gmail.com>
2024-01-15 15:56:11 +0200

    add the xml files

--- DalFacade/DO/Exceptions.cs
+++ DalFacade/DO/Exceptions.cs
@@ -1,16 +1,34 @@
-﻿namespace DO;
+﻿using System.Runtime.Serialization;
+
+namespace DO;
+
 [Serializable]
 public class DalDoesNotExistException : Exception
 {
     public DalDoesNotExistException(string? message) : base(message) { }
+    public DalDoesNotExistException(string? message, Exception? innerException) : base(message, innerException) { }
 }
 
+[Serializable]
 public class DalAlreadyExistsException : Exception
 {
     public DalAlreadyExistsException(string? message) : base(message) { }
+    public DalAlreadyExistsException(string? message, Exception? innerException) : base(message, innerException) { }
 }
 
+[Serializable]
 public class DalDeletionImpossible : Exception
 {
     public DalDeletionImpossible(string? message) : base(message) { }
+    public DalDeletionImpossible(string? message, Exception? innerException) : base(message, innerException) { }
+
+}
+
+[Serializable]
+public class DalXMLFileLoadCreateException : Exception
+{
+
+    public DalXMLFileLoadCreateException(string? message) : base(message)    { }
+    public DalXMLFileLoadCreateException(string? message, Exception? innerException) : base(message, innerException)    { }
+
 }
---
+++ DalXml/EngineerImplementation.cs
@@ -0,0 +1,38 @@
+﻿namespace Dal;
+using DalApi;
+using DO;
+
+internal class EngineerImplementation : IEngineer
+{
+    readonly string s_Engineer_xml = "students";
+
+    public int Create(Engineer item)
+    {
+        throw new NotImplementedException();
+    }
+
+    public void Delete(int id)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Engineer? Read(int id)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Engineer? Read(Func<Engineer, bool> filter = null)
+    {
+        throw new NotImplementedException();
+    }
+
+    public IEnumerable<Engineer?> ReadAll(Func<Engineer, bool> filter = null)
+    {
+        throw new NotImplementedException();
+    }
+
+    public void Update(Engineer item)
+    {
+        throw new NotImplementedException();
+    }
+}
