06e5666ac2537066fde960bd9ff7325abae02d42
hadas <hdsdwnt@gmail.com>
2024-01-23 20:42:24 +0200

    change the create and the delete in xml

--- DalXml/EngineerImplementation.cs
+++ DalXml/EngineerImplementation.cs
@@ -12,29 +12,92 @@ internal class EngineerImplementation : IEngineer
 
     public int Create(Engineer item)
     {
-        XElement engineerXML = new XElement("Enginner",
-        new XElement("Id", item.Id),
-        new XElement("Email", item.Email),
-        new XElement("Cost", item.Cost),
-        new XElement("Name", item.Name),
-        new XElement("Level", item.Level));
+        try
+        {
+            // Ensure unique ID for the new engineer
+            item.Id = GetAndIncreaseNextId(s_Engineer_xml, "NextId");
 
+            // Create the XML element for the engineer
+            XElement engineerXML = new XElement("Engineer",
+                new XElement("Id", item.Id),
+                new XElement("Email", item.Email),
+                new XElement("Cost", item.Cost),
+                new XElement("Name", item.Name),
+                new XElement("Level", item.Level)
+            );
 
-        return item.Id;
+            // Load the existing Engineers XML
+            XElement engineersXML = LoadListFromXMLElement(s_Engineer_xml);
+
+            // Append the new engineer to the list
+            engineersXML.Add(engineerXML);
+
+            // Save the updated Engineers XML
+            SaveListToXMLElement(engineersXML, s_Engineer_xml);
+
+            return item.Id;
+        }
+        catch (Exception ex)
+        {
+            // Handle exceptions appropriately
+            throw new DalXMLFileLoadCreateException("Failed to create engineer in XML: " + ex.Message);
+        }
     }
+
+    //public int Create(Engineer item)
+    //{
+    //    XElement engineerXML = new XElement("Enginner",
+    //    new XElement("Id", item.Id),
+    //    new XElement("Email", item.Email),
+    //    new XElement("Cost", item.Cost),
+    //    new XElement("Name", item.Name),
+    //    new XElement("Level", item.Level));
+
+
+    //    return item.Id;
+    //}
 
     public void Delete(int id)
     {
+        try
+        {
+            // Load the existing Engineers XML
+            XElement engineersXML = LoadListFromXMLElement(s_Engineer_xml);
 
-        var engineers = LoadListFromXMLSerializer<Engineer>(s_Engineer_xml);
+            // Find the index of the Engineer element with the specified id
+            int index = engineersXML.Elements("Engineer").ToList().FindIndex(e => e.Element("Id")!.Value == id.ToString());
 
-        int index = engineers.FindIndex(x => x.Id == id);
-        if (index == -1)
-            throw new DalDoesNotExistException("");
+            // Throw exception if engineer not found
+            if (index == -1)
+            {
+                throw new DalDoesNotExistException("Engineer with id " + id + " does not exist.");
+            }
 
-        engineers.RemoveAt(index);
-        SaveListToXMLSerializer<Engineer>(engineers, s_Engineer_xml);
+            // Remove the Engineer element at the specified index
+            engineersXML.Elements("Engineer").ElementAt(index).Remove();
+
+            // Save the updated Engineers XML
+            SaveListToXMLElement(engineersXML, s_Engineer_xml);
+        }
+        catch (Exception ex)
+        {
+            // Handle exceptions appropriately
+            throw new DalXMLFileLoadCreateException("Failed to delete engineer from XML: " + ex.Message);
+        }
     }
+
+    //public void Delete(int id)
+    //{
+
+    //    var engineers = LoadListFromXMLSerializer<Engineer>(s_Engineer_xml);
+
+    //    int index = engineers.FindIndex(x => x.Id == id);
+    //    if (index == -1)
+    //        throw new DalDoesNotExistException("");
+
+    //    engineers.RemoveAt(index);
+    //    SaveListToXMLSerializer<Engineer>(engineers, s_Engineer_xml);
+    //}
 
     public Engineer? Read(int id)
     {
