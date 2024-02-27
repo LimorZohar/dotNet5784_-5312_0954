39ecea46fa46d97f940e07974d0ad6d974ea97f1
hadas <hdsdwnt@gmail.com>
2024-02-27 16:22:11 +0200

    add the rest windows

--- PL/MainWindow.xaml.cs
+++ PL/MainWindow.xaml.cs
@@ -3,34 +3,43 @@ using PL.Engineer;
 using PL.Task;
 using System.Windows;
 
-
 namespace PL
 {
     /// <summary>
-    /// Interaction logic for MainWindow.xaml
+    /// Interaction logic for TheMainWindow.xaml
     /// </summary>
+    /// 
     public partial class MainWindow : Window
     {
-        IBl bl = Factory.Get();  
+        IBl bl = Factory.Get();
+        private void Maneger_Click(object sender, RoutedEventArgs e)
+        {
+            string userInput = Microsoft.VisualBasic.Interaction.InputBox("Please enter your Id:", "Enter Id", "214125312");
+
+            // בדיקה אם המשתמש הזין ערך ולא עזב אותו ריק
+            if (!string.IsNullOrEmpty(userInput))
+            {
+                // פתיחת חלון המנהל
+                new ManagerWindow().Show();
+            }
+
+        }
+        private void Engineer_Click(object sender, RoutedEventArgs e)
+        {
+            //// בקשה מהמשתמש להזין את המזהה שלו
+            //string userInput = Microsoft.VisualBasic.Interaction.InputBox("Please enter your Id:", "Enter Id", "214125312");
+
+            //// בדיקה אם המשתמש הזין ערך ולא עזב אותו ריק
+            //if (!string.IsNullOrEmpty(userInput))
+            //{
+                // פתיחת חלון המהנדס עם המזהה שהמשתמש הזין
+                new EngineerWindow().Show();
+            
+
+        }
         public MainWindow()
         {
             InitializeComponent();
         }
-        //Button_Click method
-        private void Button_Click(object sender, RoutedEventArgs e)
-        {
-            new EngineerListWindow().Show();
-
-        }
-        private void OpenEngineerWindow(object sender, RoutedEventArgs e) => new EngineerListWindow().Show();
-        private void OpenAdminWindow(object sender, RoutedEventArgs e) => new EngineerWindow().Show();
-
-        
-        private void InitData(object sender, RoutedEventArgs e)
-        {
-            DalTest.Initialization.Do();
-        }
-
-        private void OpenTaskWindow(object sender, RoutedEventArgs e)=> new TaskListWindows2().Show();
     }
 }
---
+++ PL/ManagerWindow.xaml.cs
@@ -0,0 +1,41 @@
+﻿using BlApi;
+using DalTest;
+using PL.Engineer;
+using PL.Task;
+using System.Windows;
+
+namespace PL
+{
+    /// <summary>
+    /// Interaction logic for ManagerWindow.xaml
+    /// </summary>
+    public partial class ManagerWindow : Window
+    {
+        IBl bl = Factory.Get();
+
+        private void OpenEngineerListWindow(object sender, RoutedEventArgs e)
+        {
+            new EngineerListWindow().Show();
+        }
+        private void OpenTaskListWindow(object sender, RoutedEventArgs e)
+        {
+            new TaskListWindows2().Show();
+        }
+        private void InitData(object sender, RoutedEventArgs e)
+        {
+            MessageBoxResult result = MessageBox.Show("האם תרצה לאתחל את הנתונים?", "Confirmation", MessageBoxButton.YesNo);
+            if (result == MessageBoxResult.Yes)
+            {
+                // הרצת פונקציה ליצירת נתונים ראשוניים
+                Initialization.Do();
+            }
+
+        }
+        public ManagerWindow()
+        {
+            InitializeComponent();
+        }
+
+
+    }
+}
